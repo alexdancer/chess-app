2215def3183068deb0f039da1425bc3b26dd8c66
Alex <alexdancer05@gmail.com>
2024-04-02 18:26:50 -0500

    some stuff

---
+++ src/components/Board.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./Board.css";
+
+const verticalAxis = [1, 2, 3, 4 , 5, 6, 7, 8,];
+const horizontalAxis = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+
+
+
+const Board = () => {
+  return (
+    <div>
+      <h1>Board</h1>
+    </div>
+  );
+} 
+
+export default Board;
