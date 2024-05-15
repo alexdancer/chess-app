b8f03b6eac4a17a2a95e48f8dcaac6964407d146
Alex <alexdancer05@gmail.com>
2024-05-15 18:31:12 -0500

    The pawns can now attack properly

--- src/components/Chessboard/Board.tsx
+++ src/components/Chessboard/Board.tsx
@@ -186,6 +186,60 @@ export default function Board() {
         Math.ceil((e.clientY - chessboard.offsetTop - 800) / 100)
       );
 
+      const currentPiece = pieces.find(p => p.x === gridX && p.y === gridY);
+      const attackedPiece = pieces.find(p => p.x === x && p.y === y);
+
+      if(currentPiece){
+        const validMove = rules.isValidMove(
+          gridX,
+          gridY,
+          x,
+          y,
+          currentPiece?.type,
+          currentPiece?.team,
+          pieces
+        );
+        // a reduce function
+        // results -> array of results
+        // piece -> current piece we are handling
+        if (validMove) {
+          // updates piece position
+          // and if a piece is attacked, removed it
+          const updatedPieces = pieces.reduce((results, piece) => {
+            if (piece.x === currentPiece.x && piece.y === currentPiece.y) {
+              piece.x = x;
+              piece.y = y;
+              results.push(piece);
+            } else if (!(piece.x === x && piece.y === y)) {
+              results.push(piece);
+            }
+
+            return results;
+          }, [] as Tile[]);
+
+          setPieces(updatedPieces)
+
+          // setPieces((value) => {
+          //   const pieces = value.reduce((results, piece) => {
+          //     if (piece.x === currentPiece.x && piece.y === currentPiece.y) {
+          //       piece.x = x;
+          //       piece.y = y;
+          //       results.push(piece);
+          //     } else if (!(piece.x === x && piece.y === y)) {
+          //       results.push(piece);
+          //     }
+
+          //     return results;
+          //   }, [] as Tile[]);
+
+          //   return pieces;
+          // });
+        } else {
+          activePiece.style.position = "relative";
+          activePiece.style.removeProperty("top");
+          activePiece.style.removeProperty("left");
+        }
+      }
       // updates the piece position
       setPieces((value) => {
         const pieces = value.map((p) => {
@@ -198,7 +252,7 @@ export default function Board() {
               p.type,
               p.team,
               value
-            );
+            )
 
             if (validMove) {
               p.x = x;
--- src/rules/Rules.tsx
+++ src/rules/Rules.tsx
@@ -2,8 +2,6 @@ import { PieceType, TeamType, Tile } from "../components/Chessboard/Board";
 
 export default class Rules {
   tileIsOccupied(x: number, y: number, boardState: Tile[]): boolean {
-    console.log("Checking if tile is occupied...");
-
     const piece = boardState.find((p) => p.x === x && p.y === y);
 
     if (piece) {
@@ -13,8 +11,14 @@ export default class Rules {
     }
   }
 
-  TileIsOccupiedByOpponent(x: number, y: number, boardState: Tile[], team: TeamType) {
-    
+  TileIsOccupiedByOpponent(x: number, y: number, boardState: Tile[], team: TeamType): boolean {
+    const piece = boardState.find((p) => p.x === x && p.y === y && p.team !== team) 
+
+    if(piece) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   isValidMove(
@@ -26,16 +30,17 @@ export default class Rules {
     team: TeamType,
     boardState: Tile[]
   ) {
-    console.log("Checking the move...");
-    console.log(`Previous location: (${prevX},${prevY})`);
-    console.log(`Current location: (${x},${y})`);
-    console.log(`Piece Type: (${type})`);
-    console.log(`Piece Type: (${type})`);
-    console.log(`Team: (${team})`);
+    // console.log("Checking the move...");
+    // console.log(`Previous location: (${prevX},${prevY})`);
+    // console.log(`Current location: (${x},${y})`);
+    // console.log(`Piece Type: (${type})`);
+    // console.log(`Piece Type: (${type})`);
+    // console.log(`Team: (${team})`);
 
     if (type === PieceType.PAWN) {
       const specialRow = (team === TeamType.WHITE) ? 1 : 6;
       const pawnDirection = (team === TeamType.WHITE) ? 1 : -1;
+      
       // movement logic
       if (
         prevX === x &&
@@ -57,9 +62,15 @@ export default class Rules {
       else if(x - prevX === -1 && y - prevY === pawnDirection) {
         // attack in upper on bottom left corner
         console.log("upper / bottom left")
+        if(this.TileIsOccupiedByOpponent(x, y, boardState, team)) {
+          return true;
+        }
       } else if(x - prevX === 1 && y - prevY === pawnDirection) {
         // attack in the upper or bottom right corner
-        console.log("upper / bottom right") 
+        console.log("upper / bottom right")
+        if(this.TileIsOccupiedByOpponent(x, y, boardState, team)) {
+          return true
+        }
       }
     }
     return false;
